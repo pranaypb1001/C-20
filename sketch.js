92408d2af6661a7c9fcce4a8fa620d3a4ecdb19d
pranaypb1001 <75839868+pranaypb1001@users.noreply.github.com>
2020-12-31 16:38:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,23 +2,29 @@ var box1,box2;
 
 function setup() {
   createCanvas(displayWidth,displayHeight);
-  box1 = createSprite(300, 300, 50, 50);
+  box1 = createSprite(800, 300, 50, 50);
   box1.shapeColor="red";
-  box2 = createSprite(400, 400, 50, 50);
+  box2 = createSprite(300, 300, 50, 50);
   box2.shapeColor="red";
+  box1.velocityX=-6
+  box2.velocityX=6
 }
 
 function draw() {
   background(0);
   box2.x = mouseX;
   box2.y = mouseY;
-  if (box2.x - box1.x <= box1.width/2 + box2.width/2 && box1.x - box2.x <= box1.width/2 + box2.width/2
-    &&  box2.y - box1.y <= box1.height/2 + box2.height/2 && box1.y - box2.y <= box1.height/2 + box2.height/2) {
-    box1.shapeColor="purple";
-    box2.shapeColor="purple";
-  } else {
-    box1.shapeColor="red";
-    box2.shapeColor="red";
+  bounceOff(box1,box2);
+  if(touch(box1,box2)){
+    stroke("white");
+    strokeWeight(2);
+    textSize(20);
+    text("hello",100,100);
+
+  }
+  else{
+  box1.debug=false;
+  box2.debug=false;
   }
   drawSprites();
 }
